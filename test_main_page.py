8c1a64109e04b78ddc65de63ce67fd8c5815c4f7
Nikolay Romeiko <nickolay.romeyko@gmail.com>
2019-11-14 22:33:50 +0200

    added test that login link is present

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,4 @@
-link = "http://selenium1py.pythonanywhere.com/"
+from pages.main_page import MainPage
 
 
 def go_to_login_page(browser):
@@ -7,6 +7,14 @@ def go_to_login_page(browser):
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # initialize Page Object, send driver instance and url to constructor
+    page.open()
+    page.go_to_login_page()
 
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
